f2c2041e76b3c10c770997c98cef076c6bce860e
Fizzah Mukhtar Mirza <58850932+fizzah-mukhtar@users.noreply.github.com>
2023-07-22 15:09:23 +0500

    Create GeneralKnowledgequizstart.cs

---
+++ GeneralKnowledgequizstart.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QUIZ_APPLICATION_PROJECT
+{
+    public partial class GKSTART : Form
+    {
+        public GKSTART()
+        {
+            InitializeComponent();
+        }
+
+        private void f1ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("THE QUIZ CONSISTS OF TOTAL 10 QUESTIONS. READ THE QUESTION CAREFULLY AND THEN SOLVE THE QUESTION. AT THE END OF THE QUIZ, YOU WILL BE NOTIFIED ABOUT YOUR RESULT.",
+"Popup Menu", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            GENERAL_KNOWLEDGE obj = new GENERAL_KNOWLEDGE();
+            obj.Show();
+            this.Hide();
+        }
+    }
+}
